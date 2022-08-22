f0816b8f3c2f130d173f59f83f87589b30fc73be
marcela <marceladefranchi59@yahoo.es>
2022-08-22 18:39:59 -0400

    SE comentacodigo de prueba de email.js por si es causa de falla en push

--- email.js
+++ email.js
@@ -1,9 +1,9 @@
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 
-let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {        
-        user: process.env.NMUSER,
-        pass: process.env.NMPASS,
-    },
-});
+// let transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {        
+//         user: process.env.NMUSER,
+//         pass: process.env.NMPASS,
+//     },
+// });
